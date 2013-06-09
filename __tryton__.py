6c3c7c21c22609054f58cb836660bfa15d0f02a8
inventiontime <inventiontime@gmail.com>
2013-06-09 10:20:32 +0800

    Adding all module files

---
+++ __tryton__.py
@@ -0,0 +1,28 @@
+#Copyright (C) Qtcollege I.P. 2012-2013
+{
+    'name': 'Qtcollege Module',
+    'version': '2.4.0.1',
+    'author': 'Qtcollege',
+    'email': 'mohammad@qtcollege.com',
+    'website': 'http://www.qtcollege.com/',
+    'description': '''
+        Party Module
+        This module will create the base functions of the Qtcollege
+    ''',
+    'depends': [
+        'ir',
+        'res',
+        'party',
+        'sale',
+        'purchase',
+        'account',
+        'account_invoice',
+        'company',
+        'stock',
+    ],
+    'xml': [
+            'college_view.xml',
+    ],
+    'translation': [
+    ],
+}
